2e24c4a609a697937dae468d5b035f00add23cd4
starrkTheDev <115180141+starrkTheDev@users.noreply.github.com>
2023-05-01 11:27:35 +0200

    Update new-game.js

--- pages/api/new-game.js
+++ pages/api/new-game.js
@@ -4,7 +4,7 @@ async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
 
-    const client = await MongoClient.connect('mongodb+srv://krystiankaczorek1:sl4b33lo@cluster0.4yy9hyb.mongodb.net/games?retryWrites=true&w=majority')
+    const client = await MongoClient.connect('mongodb+srv://krystiankaczorek1:@cluster0.4yy9hyb.mongodb.net/games?retryWrites=true&w=majority')
 
     const db = client.db();
     const gamesCollection = db.collection('games');
